69185996946eab9a35b3bd7a52631a98f9c9ab92
Ilya Petrov <dnamix1@gmail.com>
2020-05-11 17:48:57 +0300

    reconfigured navbar's UI

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -9,16 +9,16 @@ const Navbar = () => {
       alignItems="center"
       justifyContent="around"
       height={73}
-      color="midnight"
+      dangerouslySetInlineStyle={{
+        __style: {
+          backgroundColor: "#0D0221",
+          boxShadow: "rgba(0, 0, 0, 0.6) 2px 2px 9px 2px",
+        },
+      }}
       padding={1}
       margin={0}
       shape="roundedBottom"
     >
-      <NavLink activeClassName="active" to="/signin">
-        <Text size="x1" color="white">
-          Sign In
-        </Text>
-      </NavLink>
       {/* Logo & Title */}
       <NavLink activeClassName="active" exact to="/">
         <Box display="flex" alignItems="center">
@@ -30,19 +30,26 @@ const Navbar = () => {
               src="./icons/logo.svg"
             />
           </Box>
-          <Heading size="xs" color="green">
+          <Heading size="xs" color="red">
             Gear
           </Heading>
-          <Heading size="xs" color="orange">
+          <Heading size="xs" color="white">
             Buster
           </Heading>
         </Box>
       </NavLink>
-      <NavLink activeClassName="active" to="/signup">
-        <Text size="x1" color="white">
-          Sign Up
-        </Text>
-      </NavLink>
+      <Box display="flex" direction="row" width={150} justifyContent="around">
+        <NavLink activeClassName="active" to="/signin">
+          <Text size="xl" color="white" marginRight={4}>
+            Sign In
+          </Text>
+        </NavLink>
+        <NavLink activeClassName="active" to="/signup">
+          <Text size="xl" color="white">
+            Sign Up
+          </Text>
+        </NavLink>
+      </Box>
     </Box>
   );
 };
