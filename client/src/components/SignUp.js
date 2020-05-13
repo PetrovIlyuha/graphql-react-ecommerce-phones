15199b2479ecc80ee94ca2bcec0dfec34fb4add2
Ilya Petrov <dnamix1@gmail.com>
2020-05-13 12:11:44 +0300

    functional component for Sign Up process managing it's own state and connecting to strapi to sign up the user based on JWT

--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -1,7 +1,137 @@
-import React from "react";
+import React, { useState } from "react";
+import { Container, Box, Button, Heading, Text, TextField } from "gestalt";
+import ToastMessage from "./ToastMessage";
 
-const SignUp = () => {
-  return <div>Sign Up</div>;
+import { setToken } from "../utils";
+
+import Strapi from "strapi-sdk-javascript/build/main";
+const apiUrl = process.env.API_URL || "http://localhost:1337";
+const strapi = new Strapi(apiUrl);
+
+const SignUp = ({ history }) => {
+  const [userState, setUserState] = useState({
+    username: "",
+    email: "",
+    password: "",
+    toast: false,
+    message: "",
+    loading: false,
+  });
+  const handleChange = ({ event, value }) => {
+    event.persist();
+    setUserState({ ...userState, [event.target.name]: value });
+  };
+
+  const isFormEmpty = ({ username, email, password }) => {
+    return !username || !email || !password;
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    if (isFormEmpty(userState)) {
+      showToast("Fill in all Fields!");
+      return;
+    }
+    // Sign up user
+    try {
+      setUserState({ ...userState, loading: true });
+      const { username, email, password } = userState;
+      const response = await strapi.register(username, email, password);
+      setUserState({ ...userState, loading: false });
+      setToken(response.jwt);
+      console.log(response);
+      redirectUser("/");
+    } catch (err) {
+      setUserState({ ...userState, loading: false });
+      showToast(err.message);
+    }
+  };
+
+  const redirectUser = (path) => history.push(path);
+
+  const showToast = (message) => {
+    setUserState({ ...userState, toast: true, message });
+    setTimeout(() => {
+      setUserState({ ...userState, toast: false, message: "" });
+    }, 2000);
+  };
+
+  const { toast, message, loading } = userState;
+  return (
+    <Container>
+      <Box
+        dangerouslySetInlineStyle={{
+          __style: {
+            backgroundColor: "#ebe2da",
+          },
+        }}
+        margin={4}
+        padding={4}
+        shape="rounded"
+        display="flex"
+        justifyContent="center"
+      >
+        {/* Sign Up Form */}
+        <form
+          style={{
+            display: "inline-block",
+            textAlign: "center",
+            maxWidth: 450,
+          }}
+          onSubmit={handleSubmit}
+        >
+          <Box
+            marginBottom={2}
+            display="flex"
+            direction="column"
+            alignItems="center"
+          >
+            <Heading color="midnight" size="md">
+              Let's Get Going!
+            </Heading>
+            <Text italic color="orchid">
+              Sign Up to order some Phones!
+            </Text>
+          </Box>
+          {/* Username input */}
+          <TextField
+            id="username"
+            type="text"
+            name="username"
+            placeholder="Username"
+            onChange={handleChange}
+          />
+          {/* Email input */}
+          <Box marginTop={2}>
+            <TextField
+              id="email"
+              type="email"
+              name="email"
+              placeholder="Your email...We will not spam you..."
+              onChange={handleChange}
+            />
+          </Box>
+          <Box marginTop={2} marginBottom={2}>
+            <TextField
+              id="password"
+              type="password"
+              name="password"
+              placeholder="Choose your password..."
+              onChange={handleChange}
+            />
+          </Box>
+          <Button
+            disabled={loading}
+            color="red"
+            text="Sign Up"
+            type="submit"
+            inline
+          />
+        </form>
+      </Box>
+      {toast && <ToastMessage message={message} />}
+    </Container>
+  );
 };
 
 export default SignUp;
